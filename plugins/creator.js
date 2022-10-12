ea8b9566c91479fd5c4b5b5bb67f36cb3f2b5219
Aron <86765212+Chifuy@users.noreply.github.com>
2022-10-12 20:09:24 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,27 +1,24 @@
-const delay = time => new Promise(res => setTimeout(res, time))
-let handler = async(m, { conn }) => {
-	conn.p = conn.p ? conn.p : {}
-	let id = m.chat
-	conn.p[id] = [
-	await conn.sendKontak(m.chat, data.kontak, fake, { contextInfo: { externalAdReply :{
-    showAdAttribution: true,
-    mediaUrl: data.sc,
-    mediaType: 2,
-    body: 'Owner Saya Ganteng Loh Kak',
-    title: 'Nih My Owner',
-    sourceUrl: data.sc,
-    thumbnail: await(await fetch(img)).buffer(),
-     }}
-  })
-	]
-	await delay(100)
-  return conn.sendMessage(m.chat, { text: `Hay kak @${m.sender.split('@')[0]}, itu nomor ownerku jangan dispam yah ^_^`, mentions: [m.sender] }, { quoted: conn.p[id][0]
-  })
-  await delay(100)
-  return delete conn.p[id]
+const PhoneNumber = require('awesome-phonenumber')
+async function handler(m) {
+    let vcard = 'BEGIN:VCARD\n'
+    + 'VERSION:3.0\n' 
+    + 'N:;Aron;;;'
+    + 'FN:Aron\n'
+    + 'ORG:Aron-Kun;\n'
+    + 'item1.TEL;type=CELL;type=VOICE;waid=6285747543536:+62 857-4754-3536\n' 
+    + 'item1.X-ABLabel:Creator Origami-Bot\n'
+    + 'item2.EMAIL;type=INTERNET:origamibot67@gmail.com\n'
+    + 'item2.X-ABLabel:Email\n'
+    + 'item3.URL:https://www.instagram.com/hy_bee\n'
+    + 'item3.X-ABLabel:Instagram\n'
+    + 'item4.ADR:;;Kab. Brebes, Jawa Tengah, Indonesia;;;;\n'
+    + 'item4.X-ABLabel:Region\n'
+    + 'END:VCARD'
+
+conn.sendMessage(m.chat, { contacts: { displayName: 'Aron', contacts: [{ vcard }] } }, { quoted: m })
 }
 
-handler.help = ['owner']
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
 handler.command = /^(owner|creator)$/i
 
